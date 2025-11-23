224f5523a5ebdfdb2eb07b49a0afe35423813dc9
David Ott <ottdavid23@gmail.com>
2025-11-22 20:59:56 -0700

    Initial Commit

---
+++ Finance/FinanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceApp.swift
+//  Finance
+//
+//  Created by David Ott on 11/22/25.
+//
+
+import SwiftUI
+
+@main
+struct FinanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FinanceTests/FinanceTests.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceTests.swift
+//  FinanceTests
+//
+//  Created by David Ott on 11/22/25.
+//
+
+import Testing
+@testable import Finance
+
+struct FinanceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
